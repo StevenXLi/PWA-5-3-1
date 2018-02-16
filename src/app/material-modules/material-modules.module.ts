56a674d3395d8d8dec6f3981ad563a01ff5d5d43
Steven Li <stevenli_8118@hotmail.com>
2018-02-16 16:31:50 -0500

    added basic layout of main page of app

--- src/app/material-modules/material-modules.module.ts
+++ src/app/material-modules/material-modules.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule, MatCardModule, MatGridListModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatCardModule, MatGridListModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatButtonModule, MatToolbarModule, MatCardModule, MatGridListModule
+    MatButtonModule, MatToolbarModule, MatCardModule, MatGridListModule, MatIconModule
   ],
   exports: [
-    MatButtonModule, MatToolbarModule, MatCardModule, MatGridListModule
+    MatButtonModule, MatToolbarModule, MatCardModule, MatGridListModule, MatIconModule
   ]
 })
 export class MaterialModulesModule { }
